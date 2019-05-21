94ab61be4a9135929e8cd16505bf3383f5d81c92
shakimharris <amenraprod@gmail.com>
2019-05-21 15:57:27 -0400

    new click

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -5,9 +5,8 @@ import './App.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {this.props.value} === {this.props.nextplayerAPI}
-        (then.bind.this);
+      <button className="square" onClick={function() { alert('click'); }}>
+        {this.props.value}
       </button>
     );
   }
