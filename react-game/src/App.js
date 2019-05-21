366a4414e340b9424f1132c934d0342634cbacb4
shakimharris <amenraprod@gmail.com>
2019-05-21 16:07:56 -0400

    arrow func added

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={function() { alert('click'); }}>
+      <button className="square" onClick={() => { alert('click'); }}>
         {this.props.value}
       </button>
     );
